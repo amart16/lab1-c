f232576ca7e0be7682159f1269fdd7d52103922d
amart16 <arm6554@psu.edu>
2020-09-05 00:05:47 +0000

    space

--- main.c
+++ main.c
@@ -9,6 +9,6 @@ int main(void) {
   printf("Enter temperature in celsius: ");
   scanf("%f",&celsius);
   fahrenheit=(1.8*celsius)+32;
-  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n" , celsius, fahrenheit);
+  printf("\n%f° in Celsius is equivalent to %f° Fahrenheit.\n" , celsius, fahrenheit);
   return 0;
 }
